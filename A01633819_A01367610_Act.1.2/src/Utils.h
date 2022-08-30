cc346ccc25f24588a3ccc73186042a1b16e3934e
Jose Naime <josep.naimeg@gmail.com>
2022-08-30 16:45:46 -0500

    Utils class added and code refactor

---
+++ A01633819_A01367610_Act.1.2/src/Utils.h
@@ -0,0 +1,22 @@
+//
+// Created by Jose Naime on 30/08/22.
+//
+
+#ifndef ADVANCED_ALGORITHMS_UTILS_H
+#define ADVANCED_ALGORITHMS_UTILS_H
+
+
+class Utils {
+    public:
+        static mergeSort(int, int, vector<int> &);
+
+    private:
+        merge(int,
+              int,
+              vector<int> &)
+
+
+};
+
+
+#endif //ADVANCED_ALGORITHMS_UTILS_H
