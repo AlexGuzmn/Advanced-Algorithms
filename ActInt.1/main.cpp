462b613d39d3daa12ea275ef8e434aad69f7cfee
Jose Naime <josep.naimeg@gmail.com>
2022-10-04 09:59:19 -0500

    ActInt.1 input files updated

--- ActInt.1/main.cpp
+++ ActInt.1/main.cpp
@@ -1,3 +1,83 @@
-//
-// Created by Jose Naime on 09/09/22.
-//
+
+/*
+ACTIVIDAD INTEGRADORA 1
+
+Andrés Alejandro Guzmán González - A01633819
+José Pablo Naime García - A01367610
+Vanessa M. Cuevas Arroyo - A01634064
+*/
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "./src/Utils.h"
+
+using namespace std;
+
+int main() {
+    map<string, string>::iterator itr;
+    map<string, string> filesValues;
+
+    filesValues["mccode1"] = Utils::openFiles(
+            "/Users/jose_n/Library/Mobile Documents/com~apple~CloudDocs/Clion Projects/Actividad_Integradora_1/src/inputs/mccode1.txt");
+    filesValues["mccode2"] = Utils::openFiles(
+            "/Users/jose_n/Library/Mobile Documents/com~apple~CloudDocs/Clion Projects/Actividad_Integradora_1/src/inputs/mccode2.txt");
+    filesValues["mccode3"] = Utils::openFiles(
+            "/Users/jose_n/Library/Mobile Documents/com~apple~CloudDocs/Clion Projects/Actividad_Integradora_1/src/inputs/mccode3.txt");
+    filesValues["transmission1"] = Utils::openFiles(
+            "/Users/jose_n/Library/Mobile Documents/com~apple~CloudDocs/Clion Projects/Actividad_Integradora_1/src/inputs/transmission1.txt");
+    filesValues["transmission2"] = Utils::openFiles(
+            "/Users/jose_n/Library/Mobile Documents/com~apple~CloudDocs/Clion Projects/Actividad_Integradora_1/src/inputs/transmission2.txt");
+
+    /*
+     * Ejericio 1:
+     */
+
+    cout << "\n------ Ejercicio 1" << endl;
+
+    for (itr = filesValues.begin(); itr != filesValues.end(); ++itr) {
+        if (itr->first.find("mccode") != string::npos) {
+            unsigned int pos1 = 0;
+            unsigned int pos2 = 0;
+
+            string isInTransmission1 = Utils::isContentInFile(filesValues["transmission1"], itr->second, &pos1) ? "true"
+                                                                                                                : "false";
+            string isInTransmission2 = Utils::isContentInFile(filesValues["transmission2"], itr->second, &pos2) ? "true"
+                                                                                                                : "false";
+            cout << itr->first << endl;
+            cout << isInTransmission1 << " - " << "transmission1" << "\t" << pos1 << "\t" << itr->second << endl;
+            cout << isInTransmission2 << " - " << "transmission2" << "\t" << pos2 << "\t" << itr->second << endl;
+            cout << endl;
+        }
+    }
+
+    /*
+     * Ejercicio 2
+     */
+
+    cout << "\n------ Ejercicio 2" << endl;
+
+    for (itr = filesValues.begin(); itr != filesValues.end(); ++itr) {
+        unsigned int startPos = 0;
+        unsigned int endPos = 0;
+        cout << itr->first << " - " << Utils::findLongestPalindrome(itr->second, &startPos, &endPos) << " - Pos: " << startPos << " " << endPos<< endl;
+
+    }
+
+    /*
+     * Ejercicio 3
+     */
+    cout << "\n------ Ejercicio 3" << endl;
+    unsigned int startPos = 0;
+    unsigned int endPos = 0;
+    Utils::longestCommonSubstring(filesValues["transmission1"], filesValues["transmission2"], &startPos, &endPos);
+    cout << startPos << " " << endPos << endl;
+
+
+
+
+
+
+    return 0;
+}
--- ActInt.1/src/Utils.h
+++ ActInt.1/src/Utils.h
@@ -5,22 +5,28 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <iostream>
-#include <string>
 #include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
 
 class Utils {
     public:
         static string openFiles(string fileName);
 
-        static string findLongestPalindrome(string str);
+        static string findLongestPalindrome(string str, unsigned int *startPos, unsigned int *endPos);
+        static bool isContentInFile(string fileContent, string contentToFind, unsigned int *pos);
+        static void longestCommonSubstring(string string1, string string2, unsigned int *startPos, unsigned int *endPos);
 
     private:
-        static string getLongestPalindromeFromMatrix(int **str, int maxCenter, int maxLen);
-
+        static string getLongestPalindromeFromMatrix(string str, int maxCenter,
+                                                     int maxLen);
+        static void computeLPSArray(string pat, int M, int *lps);
 };
 
-string Utils::openFiles(string srcFile){
+string Utils::openFiles(string srcFile) {
     string line;
     string fileContent = "";
     ifstream file(srcFile);
@@ -35,15 +41,94 @@ string Utils::openFiles(string srcFile){
     return fileContent;
 }
 
-string Utils::findLongestPalindrome(string str) {
-    int center = 0;
-    int left = 0;
-    int right = 0;
-    int maxLen = 0;
-    int maxCenter = 0;
-    int p[str.length()];
+// KMP algorithm to verify if a string is inside another string
+// Time complexity: O(n)
+bool Utils::isContentInFile(string fileContent, string contentToFind, unsigned int *pos) {
+    int m = contentToFind.length();
+    int n = fileContent.length();
 
-    for (int i = 1; i < len - 1; i++) {
+    int lps[m];
+    computeLPSArray(contentToFind, m, lps);
+
+    int j = 0;
+    int i = 0;
+    while (i < n) {
+        if (contentToFind[j] == fileContent[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == m) {
+            *pos = i - j;
+            return true;
+        } else if (i < n && contentToFind[j] != fileContent[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i = i + 1;
+            }
+        }
+    }
+    return false;
+}
+
+// Time complexity: O(n)
+void Utils::computeLPSArray(string pat, int M, int *lps) {
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+// Time Complexity O(n^2)
+void Utils::longestCommonSubstring(std::string string1, std::string string2, unsigned int *startPos, unsigned int *endPos) {
+    int m = string1.length();
+    int n = string2.length();
+
+    int longestCommonSubstring[m + 1][n + 1];
+    int longestSubstringLength = 0;
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0) {
+                longestCommonSubstring[i][j] = 0;
+            } else if (string1[i - 1] == string2[j - 1]) {
+                longestCommonSubstring[i][j] = longestCommonSubstring[i - 1][j - 1] + 1;
+                if (longestCommonSubstring[i][j] > longestSubstringLength) {
+                    longestSubstringLength = longestCommonSubstring[i][j];
+                    *startPos = i - longestSubstringLength;
+                    *endPos = i;
+                }
+            } else {
+                longestCommonSubstring[i][j] = 0;
+            }
+        }
+    }
+}
+
+// Time Complexity O(n^2) as we are making a matrix of size n^2 and we are iterating over it
+string Utils::findLongestPalindrome(string str, unsigned int *startPos, unsigned int *endPos) {
+    unsigned int center = 0;
+    unsigned int right = 0;
+    unsigned int maxLen = 0;
+    unsigned int maxCenter = 0;
+    int p[100] = {0};
+
+    for (int i = 1; i < str.length() - 1; i++) {
         int mirror = 2 * center - i;
         if (right > i) {
             p[i] = (right - i) < p[mirror] ? (right - i) : p[mirror];
@@ -63,19 +148,23 @@ string Utils::findLongestPalindrome(string str) {
         }
     }
 
+    *startPos = maxCenter - maxLen + 1;
+    *endPos = maxCenter + maxLen + 1;
+
     return getLongestPalindromeFromMatrix(str, maxCenter, maxLen);
 }
 
-string Utils::getLongestPalindromeFromMatrix(int **str, int maxCenter, int maxLen) {
-    string longestPalindrome = "";
+// Time Complexity O(n)
+string Utils::getLongestPalindromeFromMatrix(string str, int maxCenter,
+                                             int maxLen) {
+    string res = "";
 
     // Print the longest palindrome and add it to the string
     for (int i = maxCenter - maxLen; i <= maxCenter + maxLen; i++) {
-        longestPalindrome += str[i];
-        cout << str[i];
+        res += str[i];
     }
 
-    return longestPalindrome;
+    return res;
 }
 
-#endif //UTILS_H
+#endif // UTILS_H
